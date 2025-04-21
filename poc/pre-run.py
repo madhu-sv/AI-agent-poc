189c99ea2e2544a3e2464b13eb542d9139f0ac07
Madhusudhan V <madhusudhan.sv@gmail.com>
2025-04-21 12:28:44 +0100

    First commit

---
+++ poc/pre-run.py
@@ -0,0 +1,5 @@
+import nltk
+
+nltk.download('stopwords', download_dir='/usr/local/share/nltk_data')
+nltk.download('punkt', download_dir='/usr/local/share/nltk_data')
+nltk.download("punkt_tab", download_dir="/usr/local/share/nltk_data")
